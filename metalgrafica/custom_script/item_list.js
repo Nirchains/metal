9ed95267817bb0cb6768c33bfc95b67ed4a175d3
Nirchains <nirchains@gmail.com>
2017-03-31 14:59:21 +0200

    fixtures

--- metalgrafica/custom_script/item_list.js
+++ metalgrafica/custom_script/item_list.js
@@ -1,3 +1,10 @@
+frappe.listview_settings['Item']['onload'] =
+    function(listview) {
+		frappe.route_options = {
+			"has_variants": "No"
+		};
+	}
+
 frappe.listview_settings['Item']['refresh'] =
 	function(me) {
 		// Filtros
@@ -11,7 +18,7 @@ frappe.listview_settings['Item']['refresh'] =
         me.page.add_sidebar_item(__("Productos"), function() {
 			var publish_filter = me.filter_list.get_filter("has_variants");
 			publish_filter && publish_filter.remove(true);
-			me.filter_list.add_filter(me.doctype, "has_variants", '!=', "Yes");
+			me.filter_list.add_filter(me.doctype, "has_variants", '=', "No");
 			me.run();
 		});
 	}
