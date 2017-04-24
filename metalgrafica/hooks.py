513b6b4da69536b362c699b3382c928304318d94
Nirchains <nirchains@gmail.com>
2017-04-24 14:54:05 +0200

    Ellipsis

--- metalgrafica/hooks.py
+++ metalgrafica/hooks.py
@@ -15,8 +15,14 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = ["/assets/metalgrafica/css/material.css", "/assets/metalgrafica/css/custom.css"]
-app_include_js = ["/assets/metalgrafica/js/custom.js"]
+app_include_css = [
+	"/assets/metalgrafica/css/material.css", 
+	"/assets/metalgrafica/css/custom.css"
+]
+app_include_js = [
+	"/assets/metalgrafica/js/custom.js",
+	"assets/js/templates.min.js"
+]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/metalgrafica/css/metalgrafica.css"
