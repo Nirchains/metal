5f890b6fe74e439d556fa300349fd027254042f1
Nirchains <nirchains@gmail.com>
2018-06-08 14:17:28 +0200

    m

--- metalgrafica/custom_script/sales_order.js
+++ metalgrafica/custom_script/sales_order.js
@@ -5,6 +5,13 @@ frappe.ui.form.on('Sales Order', {
 		frm.toggle_reqd("source", helper.In(frm.doc.customer,["AUXIMARA,S.A."]));
 
 		frm.refresh_fields();
+
+		//Forzar que el formato de impresión sea siempre el mismo
+		frm.set_value('letter_head','Estándar');
+	},
+
+	letter_head: function(frm) {
+		
 	}
 	
 });
